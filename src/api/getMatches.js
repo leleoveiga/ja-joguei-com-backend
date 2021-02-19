e2d5a39cb963f8fe93ca9748cc09db15fa2eee74
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-18 22:12:11 -0300

    refactoring

--- src/api/getMatches.js
+++ src/api/getMatches.js
@@ -9,36 +9,19 @@ router.get("/:nick1/:nick2", async (req, res, next) => {
 
     // link api
     let urlTarget =
-      "https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/?id1?api_key=?apiKey&endIndex=50";
-    urlTarget = urlTarget.replace("?apiKey", process.env.API_KEY);
+      "https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/?id0?api_key=?apiKey&endIndex=50";
 
-    urlTarget = urlTarget.replace("?id1", ids[0]);
     // coloca a api key e id do player no link
+    urlTarget = urlTarget.replace("?apiKey", process.env.API_KEY);
+    urlTarget = urlTarget.replace("?id0", ids[0]);
 
     // pega a lista de partidas do player
     const { data } = await axios.get(urlTarget);
 
-    const matches = [];
-    for (let i = 0; i < data.matches.length; i++) {
-      matches.push(data.matches[i].gameId);
-    }
-
     // filtra com o segundo id
-    const foundMatches = await playedWith(ids[0], ids[1], matches);
+    const foundMatches = await playedWith(ids[0], ids[1], data.matches);
 
-    const matchesObject = [];
-    foundMatches.forEach(function (gameIdFound, index, array) {
-      data.matches.forEach(function (match, index, arrayAll) {
-        if (match.gameId === gameIdFound) {
-          matchesObject.push(match);
-          // matchesObject.push(
-          //   `https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/${gameIdFound}?tab=overview`
-          // );
-        }
-      });
-    });
-
-    res.json(matchesObject);
+    res.json(foundMatches);
   } catch (error) {
     next(error);
   }
@@ -62,25 +45,43 @@ async function playedWith(id1, id2, matches) {
 
   for (let i = 0; i < matches.length; i++) {
     console.log(
-      `-------------------match: ${i + 1}----------------------------`
+      `---------------------match: ${i + 1}--------------------------`
     );
     urlMatch =
       "https://br1.api.riotgames.com/lol/match/v4/matches/?matchId?api_key=?apiKey";
     urlMatch = urlMatch.replace("?apiKey", process.env.API_KEY);
-    urlMatch = urlMatch.replace("?matchId", matches[i]);
+    urlMatch = urlMatch.replace("?matchId", matches[i].gameId);
 
     // get partida pelo id
     const { data } = await axios.get(urlMatch);
 
-    const players = data.participantIdentities;
-    for (let j = 0; j < players.length; j++) {
-      console.log(players[j].player.accountId);
-      if (players[j].player.accountId === id2) {
     // pega a lista dos jogadores da partida
+    const participantIdentities = data.participantIdentities;
+
     // add as partidas em q id2 aparece
+    for (let j = 0; j < participantIdentities.length; j++) {
+      if (participantIdentities[j].player.accountId === id2) {
+        console.log(
+          "////////////////////////match found!////////////////////////"
+        );
+        const championName1 = await getChampionNameByKey(matches[i].champion);
+        const championIcon1 = getChampionIconLinkByName(championName1);
+        const participant2Id = participantIdentities[j].participantId;
+        const championKey2 = data.participants[participant2Id - 1].championId;
+        const championName2 = await getChampionNameByKey(championKey2);
+        const championIcon2 = getChampionIconLinkByName(championName2);
+
+        matches[i].icon1 = championIcon1;
+        matches[i].icon2 = championIcon2;
+        matches[
+          i
+        ].link = `https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/${matches[i].gameId}?tab=overview`;
         foundMatches.push(matches[i]);
       }
     }
+    console.log(
+      `https://matchhistory.br.leagueoflegends.com/pt/#match-details/BR1/${matches[i].gameId}?tab=overview`
+    );
   }
 
   return foundMatches;
