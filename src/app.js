440d5b445d70caea2e9ef6ae8e99ada92f991928
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-25 17:22:47 -0300

    add morgan and helmet

--- src/app.js
+++ src/app.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv/config");
-// const morgan = require("morgan");
-// const helmet = require("helmet");
-
+const morgan = require("morgan");
+const helmet = require("helmet");
 const middlewares = require("./middlewares");
 const api = require("./api");
 
 const app = express();
-app.set("trust proxy", 1);
 
-// app.use(morgan("dev"));
-// app.use(helmet());
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+    message: "👋🌎🌍🌏 home :)",
   });
 });
 
 app.use("/api/", api);
-
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
