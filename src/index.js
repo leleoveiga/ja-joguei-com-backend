2444abd529965eca4422f64a8fc0d10d7bfc3afd
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-25 21:16:54 -0300

    production steps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const app = require("./app");
+const path = require("path");
+
+// production
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(path.join(__dirname, "../public/")));
+
+  // handle spa
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname + "../public/index.html"));
+  });
+}
 
 const port = 5000;
 
