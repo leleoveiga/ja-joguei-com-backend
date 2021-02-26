83d50e334d0becbadd6f519154f5933868226cc6
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-02-26 08:12:49 -0300

    deploy config tests

--- src/app.js
+++ src/app.js
@@ -8,18 +8,32 @@ const api = require("./api");
 
 const app = express();
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/", api);
+// production
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(path.join(__dirname, "../public/")));
+
+  // handle spa
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "../public/index.html");
+  });
+}
 
 app.get("/", (req, res) => {
   res.json({
     message: "👋🌎🌍🌏 home :)",
   });
 });
-
-app.use("/api/", api);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
